dd76dc8820a59c8325a86a2f7825eaf6592d3555
julenvitoria <36037023+julenvitoria@users.noreply.github.com>
2020-08-12 21:18:50 +0200

    Update ThemePlusLaunchings.sh

--- ThemePlusLaunchings.sh
+++ ThemePlusLaunchings.sh
@@ -2,7 +2,8 @@
 
 cd ~
 if [ -d "/home/pi/.emulationstation/themes/Super Retroboy/" ]; then
-        echo "Super Retroboy theme was downloaded yet. Deleting and redownloading..."
+        echo "Super Retroboy theme was downloaded yet."
+        echo "Deleting and redownloading..."
         sleep 4
         sudo rm -r "/home/pi/.emulationstation/themes/Super Retroboy/"
         sudo mkdir -p "/home/pi/.emulationstation/themes/Super Retroboy/" && sudo git clone https://github.com/KALEL1981/Super_Retroboy_Theme.git "/home/pi/.emulationstation/themes/Super Retroboy/" --branch master --depth=1
