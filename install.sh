65e981f7f5bc03e73051f9d265259c56902b5cf5
julenvitoria <36037023+julenvitoria@users.noreply.github.com>
2020-08-12 20:43:46 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-git clone
+if [ -d /home/pi/.emulationstation/themes/Super Retroboy/ ]; then
+        echo "Directory update addons was created yet. Deleting and redownloading..."
+        sudo rm -r "/home/pi/.emulationstation/themes/Super Retroboy/"
+else
+        sudo mkdir -p "/home/pi/.emulationstation/themes/Super Retroboy/" && sudo git clone https://github.com/KALEL1981/Super_Retroboy_Theme.git "/home/pi/.emulationstation/themes/Super Retroboy/" --branch master --depth=1
+fi
