2590ee8dee7b8c857ad0fdcf90bf079e4adf9cdb
julenvitoria <36037023+julenvitoria@users.noreply.github.com>
2020-08-12 21:15:13 +0200

    Update ThemePlusLaunchings.sh

--- ThemePlusLaunchings.sh
+++ ThemePlusLaunchings.sh
@@ -3,8 +3,12 @@
 cd ~
 if [ -d "/home/pi/.emulationstation/themes/Super Retroboy/" ]; then
         echo "Directory update addons was created yet. Deleting and redownloading..."
+        sleep 4
         sudo rm -r "/home/pi/.emulationstation/themes/Super Retroboy/"
+        sudo mkdir -p "/home/pi/.emulationstation/themes/Super Retroboy/" && sudo git clone https://github.com/KALEL1981/Super_Retroboy_Theme.git "/home/pi/.emulationstation/themes/Super Retroboy/" --branch master --depth=1
 else
+        echo "Directory update addons was created yet. Deleting and redownloading..."
+        sleep 4
         sudo mkdir -p "/home/pi/.emulationstation/themes/Super Retroboy/" && sudo git clone https://github.com/KALEL1981/Super_Retroboy_Theme.git "/home/pi/.emulationstation/themes/Super Retroboy/" --branch master --depth=1
 fi
 mkdir tmp
